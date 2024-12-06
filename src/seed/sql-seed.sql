66acc1049241e166a25a6c32c442f7e418c5e564
Shahryar <shahryar.sabouri.1301@gmail.com>
2024-12-06 10:57:34 +0100

    task examples in seed file

--- src/seed/sql-seed.sql
+++ src/seed/sql-seed.sql
@@ -16,3 +16,21 @@ CREATE TABLE task (
     list_id int DEFAULT NULL,
     CONSTRAINT fk_list_id FOREIGN KEY (list_id) REFERENCES list(id)
 );
+
+INSERT INTO list (title) VALUES
+('My list'),
+('Another list'),
+('A third list');
+
+INSERT INTO task (title, list_id) VALUES
+('My task', 1),
+('My second task', 1),
+('My third task', 1),
+
+('Another task', 2),
+('Yet another task', 2),
+('Tasks galore', 2),
+
+('Now you just trippin', 3),
+('You know you too lazy for all this', 3),
+('GTFO', 3);

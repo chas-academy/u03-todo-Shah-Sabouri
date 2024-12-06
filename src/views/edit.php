69806972ae509c0c5b70639bef66e66d14fc107f
Shahryar <shahryar.sabouri.1301@gmail.com>
2024-12-06 11:50:03 +0100

    edit file coding commenced

---
+++ src/views/edit.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '../db.php';
+require '../functions/crud-functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/style.css">
+    <title>Funky ass todo app</title>
+</head>
+    <div class="card">
+        <h1>Edit To Do</h1>
+        
+    </div>
+<body>
+    <
+</body>
+</html>

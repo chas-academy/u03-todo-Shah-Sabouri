cad659233207d58a1fdefc6d2e123ce2741c29f8
Shahryar <shahryar.sabouri.1301@gmail.com>
2024-12-10 16:07:58 +0100

    delete functions added into crud

--- src/functions/crud-functions.php
+++ src/functions/crud-functions.php
@@ -13,3 +13,13 @@ function getLists($conn)
     $stmt = $conn->query('SELECT id, title FROM list');
     return $stmt->fetchAll();
 }
+
+function deleteTask($conn, $task_id) {
+    $query = "DELETE FROM tasks WHERE id = :task_id";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt = bindParam(':task_id', $task_id, PDO::PARAM_INT);
+
+    return $stms->execute();
+}

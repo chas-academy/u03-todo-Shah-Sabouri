4745e8ade2e909053314d9351c240059584b4db5
Shahryar <shahryar.sabouri.1301@gmail.com>
2024-12-10 15:35:36 +0100

    .list-container coded in css

--- src/index.php
+++ src/index.php
@@ -14,7 +14,7 @@ include './functions/crud-functions.php'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="shahryar">
-    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="/src/style/style.css">
     <title>Funky ass todo app</title>
 </head>
 

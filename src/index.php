a311777c176e68caa83d545da3cd9f3ac4e133c2
Shahryar <shahryar.sabouri.1301@gmail.com>
2024-12-17 13:09:39 +0100

    more statements written in index

--- src/index.php
+++ src/index.php
@@ -16,39 +16,52 @@ include 'db.php';
     <div class="main-section">
         <div class="add-section">
             <form action="">
+                <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error'){ ?>
                     <input type="text"
                             name="title"
                             placeholder="Required field"/>
-                    <button type="submit">Add</button>
+                <button type="submit">Add</button>
+
+                <?php } else { ?>
+                <input type="text"
+                        name="title"
+                        placeholder="Add task" />
+                <button type="submit">Add</button>
+                <?php } ?>
             </form>
         </div>
         <div class="show-todo-section">
-            <div class="todo-item">
-                <input type="checkbox">
-                <h2>Random</h2>
+            <?php if ($todos->rowCount() <= 0) { ?>
+                <div class="todo-item">
+                    <div class="empty">
+                        <img src="" alt="Pic 1" width="100%" />
+                        <img src="" alt="Pic 1" width="80px">
+                    </div>
+                </div>
+            <?php } ?>
+
+            <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+                <div class="todo-item">
+                    <span id="<?php echo $todo['id']; ?>"
+                        class="remove-todo">x</span>
+                    <?php if ($todo['checked']) { ?>
+                        <input type="checkbox"
+                                class="check-box"
+                                data-todo-id="<?php echo $todo['id']; ?>"
+                                checked />
+                        <h2 class="checked">
+                    <?php echo $todo['title'] ?></h2>
+                <?php } else { ?>
+                    <input type="checkbox"
+                            data-todo-id ="<?php echo $todo['id']; ?>"
+                            class="check-box" />
+                    <h2><?php echo $todo['title'] ?></h2>
+                <?php } ?>
                 <br>
-                <small>created: Dec 17, 2024</small>
+                <small>created: <?php echo $todo['date_time'] ?></small>
             </div>
-        </div>
-        <div class="todo-item">
-            <span id="<?php echo $todo['id']; ?>"
-            class="remove-todo">x</span>
-        <?php if ($todo['checked']) { ?>
-            <input type="checkbox"
-                    class="check-box"
-                    data-todo-id="<?php echo $todo['id']; ?>"
-                    checked />
-            <h2 class="checked">
-                <?php echo $todo['title'] ?></h2>
-        <?php } else { ?>
-            <input type="checkbox"
-                    data-todo-id ="<?php echo $todo['id']; ?>"
-                    class="check-box" />
-            <h2><?php echo $todo['title'] ?></h2>
         <?php } ?>
-        <br>
-        <small>created: <?php echo $todo['date_time'] ?></small>
-        </div>
     </div>
+</div>
 </body>
 </html>

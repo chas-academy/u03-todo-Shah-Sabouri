d3e2977df3562a5736beca062a2e2725034d3f43
Shahryar <shahryar.sabouri.1301@gmail.com>
2024-12-17 12:48:49 +0100

    misc statements in index

--- src/index.php
+++ src/index.php
@@ -16,10 +16,10 @@ include 'db.php';
     <div class="main-section">
         <div class="add-section">
             <form action="">
-                <input type="text"
-                        name="title"
-                        placeholder="Required field"/>
-                <button type="submit">Add</button>
+                    <input type="text"
+                            name="title"
+                            placeholder="Required field"/>
+                    <button type="submit">Add</button>
             </form>
         </div>
         <div class="show-todo-section">
@@ -29,6 +29,25 @@ include 'db.php';
                 <br>
                 <small>created: Dec 17, 2024</small>
             </div>
+        </div>
+        <div class="todo-item">
+            <span id="<?php echo $todo['id']; ?>"
+            class="remove-todo">x</span>
+        <?php if ($todo['checked']) { ?>
+            <input type="checkbox"
+                    class="check-box"
+                    data-todo-id="<?php echo $todo['id']; ?>"
+                    checked />
+            <h2 class="checked">
+                <?php echo $todo['title'] ?></h2>
+        <?php } else { ?>
+            <input type="checkbox"
+                    data-todo-id ="<?php echo $todo['id']; ?>"
+                    class="check-box" />
+            <h2><?php echo $todo['title'] ?></h2>
+        <?php } ?>
+        <br>
+        <small>created: <?php echo $todo['date_time'] ?></small>
         </div>
     </div>
 </body>
